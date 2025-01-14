9a7f2e7c4a4e70858d33712b19ae4b2775435e99
David Hocking <65630382+KiwiBandicoot@users.noreply.github.com>
2025-01-15 12:18:02 +1300

    update

--- target/debug/deps/hellow_world-9a67505bd78a973d.d
+++ target/debug/deps/hellow_world-9a67505bd78a973d.d
@@ -1,5 +1,5 @@
-C:\Users\david\Dev\Rust\hellow_world\target\debug\deps\libhellow_world-9a67505bd78a973d.rmeta: src/main.rs
+C:\Users\david\Dev\Rust\Hello-World-Rust\target\debug\deps\libhellow_world-9a67505bd78a973d.rmeta: src/main.rs
 
-C:\Users\david\Dev\Rust\hellow_world\target\debug\deps\hellow_world-9a67505bd78a973d.d: src/main.rs
+C:\Users\david\Dev\Rust\Hello-World-Rust\target\debug\deps\hellow_world-9a67505bd78a973d.d: src/main.rs
 
 src/main.rs:
